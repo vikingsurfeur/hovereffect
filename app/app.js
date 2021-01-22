70fc8838e6a0ed663ce5ed522056768224aed74d
David Bouscarle <d.bouscarle@gmail.com>
2021-01-22 13:24:19 +0100

    ajout logo escarvaillac

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 new hoverEffect ({
     parent: document.querySelector('.container'),
-    intensity1: 0.5,
-    intensity2: 0.5,
-    speedIn: 1,
+    intensity1: 2,
+    intensity2: 2,
+    speedIn: 0.5,
     speedOut: 1,
-    image1: './img/image_1.jpg',
-    image2: './img/image_2.jpg',
+    image1: './img/escarvaillac_1.jpg',
+    image2: './img/escarvaillac_2.jpg',
     displacementImage: './img/displacement.png'
 })
