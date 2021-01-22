0ddc9bc69fbbf2a85d298ab02a22f430d0d57ea8
David Bouscarle <d.bouscarle@gmail.com>
2021-01-22 11:15:45 +0100

    initial comit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+new hoverEffect ({
+    parent: document.querySelector('.container'),
+    intensity1: 1,
+    intensity2: 2,
+    speedIn: 1,
+    speedOut: 1,
+    image1: './img/image_1.jpg',
+    image2: './img/image_2.jpg',
+    displacementImage: './img/displacement.png'
+})
